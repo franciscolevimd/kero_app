6c8b96f2c0ce2e99af12cdec211df259304aec40
Francisco Leví <francisco.levi.md@gmail.com>
2022-12-08 13:17:37 -0600

    Tests of the component that decrypt files.

--- test_kero.py
+++ test_kero.py
@@ -4,3 +4,4 @@ import doctest
 doctest.testfile('tests/utils/files.txt', verbose=False)
 doctest.testfile('tests/crypto/keys.txt', verbose=False)
 doctest.testfile('tests/crypto/encryption.txt', verbose=False)
+doctest.testfile('tests/crypto/decryption.txt', verbose=True)
